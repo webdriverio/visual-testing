9c1d20b4254c57e99190c7234a401cab13c5e035
Ivan Balukov <ivan@balukov.com>
2020-02-28 13:21:37 +0300

    Add typing for option 'returnAllCompareData: true'

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,43 +1,66 @@
 declare module WebdriverIO {
-    interface Browser {
-        /**
-         * Saves an image of an element
-         */
-        saveElement(element: Element, tag: string, saveElementOptions?: {}): {fileName: string, path: string, devicePixelRatio: number};
+  interface Browser {
+    /**
+     * Saves an image of an element
+     */
+    saveElement(
+      element: Element,
+      tag: string,
+      saveElementOptions?: {}
+    ): { fileName: string; path: string; devicePixelRatio: number };
 
-        /**
-         * Saves an image of a viewport
-         */
-        saveScreen(tag: string, saveScreenOptions?: {}): {fileName: string, path: string, devicePixelRatio: number};
+    /**
+     * Saves an image of a viewport
+     */
+    saveScreen(tag: string, saveScreenOptions?: {}): { fileName: string; path: string; devicePixelRatio: number };
 
-        /**
-         * Saves an image of the complete screen
-         */
-        saveFullPageScreen(tag: string, saveFullPageScreenOptions?: {}): {fileName: string, path: string, devicePixelRatio: number};
+    /**
+     * Saves an image of the complete screen
+     */
+    saveFullPageScreen(
+      tag: string,
+      saveFullPageScreenOptions?: {}
+    ): { fileName: string; path: string; devicePixelRatio: number };
 
-        /**
-         * Saves an image of the complete screen with the tabbable lines and dots
-         */
-        saveTabbablePage(tag: string, saveTabbableOptions?: {}): {fileName: string, path: string, devicePixelRatio: number};
+    /**
+     * Saves an image of the complete screen with the tabbable lines and dots
+     */
+    saveTabbablePage(
+      tag: string,
+      saveTabbableOptions?: {}
+    ): { fileName: string; path: string; devicePixelRatio: number };
 
-        /**
-         * Compare an image of an element
-         */
-        checkElement(element: Element, tag: string, checkElementOptions?: {}): number;
+    /**
+     * Compare an image of an element
+     */
+    checkElement(
+      element: Element,
+      tag: string,
+      checkElementOptions?: {}
+    ): number | { fileName: string; folders: { actual: string; baseline: string }; misMatchPercentage: number };
 
-        /**
-         * Compares an image of a viewport
-         */
-        checkScreen(tag: string, checkScreenOptions?: {}): number;
+    /**
+     * Compares an image of a viewport
+     */
+    checkScreen(
+      tag: string,
+      checkScreenOptions?: {}
+    ): number | { fileName: string; folders: { actual: string; baseline: string }; misMatchPercentage: number };
 
-        /**
-         * Compares an image of the complete screen
-         */
-        checkFullPageScreen(tag: string, checkFullPageOptions?: {}): number;
+    /**
+     * Compares an image of the complete screen
+     */
+    checkFullPageScreen(
+      tag: string,
+      checkFullPageOptions?: {}
+    ): number | { fileName: string; folders: { actual: string; baseline: string }; misMatchPercentage: number };
 
-        /**
-         * Compares an image of the complete screen with the tabbable lines and dots
-         */
-        checkTabbablePage(tag: string, checkTabbableOptions?: {}): number;
-    }
+    /**
+     * Compares an image of the complete screen with the tabbable lines and dots
+     */
+    checkTabbablePage(
+      tag: string,
+      checkTabbableOptions?: {}
+    ): number | { fileName: string; folders: { actual: string; baseline: string }; misMatchPercentage: number };
+  }
 }
