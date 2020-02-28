54bff58095032c44ab03a1835ca2afed530fb5ee
Ivan Balukov <ivan@balukov.com>
2020-02-28 12:09:49 +0300

    Add types for functions

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,41 +3,41 @@ declare module WebdriverIO {
         /**
          * Saves an image of an element
          */
-        saveElement(element: Element, tag: string, saveElementOptions?: {});
+        saveElement(element: Element, tag: string, saveElementOptions?: {}): void;
 
         /**
          * Saves an image of a viewport
          */
-        saveScreen(tag: string, saveScreenOptions?: {});
+        saveScreen(tag: string, saveScreenOptions?: {}): void;
 
         /**
          * Saves an image of the complete screen
          */
-        saveFullPageScreen(tag: string, saveFullPageScreenOptions?: {});
+        saveFullPageScreen(tag: string, saveFullPageScreenOptions?: {}): void;
 
         /**
          * Saves an image of the complete screen with the tabbable lines and dots
          */
-        saveTabbablePage(tag: string, saveTabbableOptions?: {});
+        saveTabbablePage(tag: string, saveTabbableOptions?: {}): void;
 
         /**
          * Compare an image of an element
          */
-        checkElement(element: Element, tag: string, checkElementOptions?: {});
+        checkElement(element: Element, tag: string, checkElementOptions?: {}): number;
 
         /**
          * Compares an image of a viewport
          */
-        checkScreen(tag: string, checkScreenOptions?: {});
+        checkScreen(tag: string, checkScreenOptions?: {}): number;
 
         /**
          * Compares an image of the complete screen
          */
-        checkFullPageScreen(tag: string, checkFullPageOptions?: {});
+        checkFullPageScreen(tag: string, checkFullPageOptions?: {}): number;
 
         /**
          * Compares an image of the complete screen with the tabbable lines and dots
          */
-        checkTabbablePage(tag: string, checkTabbableOptions?: {});
+        checkTabbablePage(tag: string, checkTabbableOptions?: {}): number;
     }
 }
