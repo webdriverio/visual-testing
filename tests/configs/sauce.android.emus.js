caa06d071ea0b4baf3d304f1bee2700bbb42c453
Wim Selles <wim@wswebcreation.nl>
2022-04-03 21:42:22 +0200

    chore: update deps
    - fix iOS tests
    - make appium caps W3C
    - udpate doc with async code

--- tests/configs/sauce.android.emus.js
+++ tests/configs/sauce.android.emus.js
@@ -1,69 +1,94 @@
-import {join} from 'path';
+const { join } = require("path");
 
-export function sauceAndroidEmus({buildName}) {
-    const mobileSpecs = join(process.cwd(), './tests/specs/mobile.spec.js');
+module.exports = function sauceAndroidEmus({ buildName }) {
+    const mobileSpecs = join(process.cwd(), "./tests/specs/mobile.spec.js");
 
     return [
         /**
          * Android with nativeWebScreenshot
          */
         {
-            deviceName: 'Google Pixel GoogleAPI Emulator',
-            browserName: 'chrome',
-            logName: 'GooglePixelGoogleAPIEmulator8.1NativeWebScreenshot',
-            platformName: 'Android',
-            platformVersion: '8.1',
-            appiumVersion: '1.18.1',
-            specs: [ mobileSpecs ],
-            nativeWebScreenshot: true,
-            build: buildName,
+            "appium:deviceName": "Google Pixel GoogleAPI Emulator",
+            browserName: "chrome",
+            platformName: "Android",
+            "appium:platformVersion": "8.1",
+            "appium:automationName": "UIAutomator2",
+            "appium:nativeWebScreenshot": true,
+            "wdio-ics:options": {
+                logName: "GooglePixelGoogleAPIEmulator8.1NativeWebScreenshot",
+            },
+            "sauce:options": {
+                appiumVersion: "1.18.1",
+                build: buildName,
+            },
+            specs: [mobileSpecs],
         },
-        ...['9.0', '10.0', '11.0'].map(platformVersion => ({
-            deviceName: 'Google Pixel 3 XL GoogleAPI Emulator',
-            browserName: 'chrome',
-            logName: `EmulatorGooglePixel3XLGoogleAPI${platformVersion}NativeWebScreenshot`,
-            platformName: 'Android',
-            platformVersion: platformVersion,
-            specs: [ mobileSpecs ],
-            nativeWebScreenshot: true,
-            build: buildName,
+        ...["9.0", "10.0", "11.0"].map((platformVersion) => ({
+            browserName: "chrome",
+            platformName: "Android",
+            "appium:deviceName": "Google Pixel 3 XL GoogleAPI Emulator",
+            "appium:platformVersion": platformVersion,
+            "appium:automationName": "UIAutomator2",
+            "appium:nativeWebScreenshot": true,
+            "wdio-ics:options": {
+                logName: `EmulatorGooglePixel3XLGoogleAPI${platformVersion}NativeWebScreenshot`,
+            },
+            "sauce:options": {
+                build: buildName,
+            },
+            specs: [mobileSpecs],
         })),
 
         /**
          * Android with chrome driver screenshots
          */
         {
-            deviceName: 'Samsung Galaxy S9 WQHD GoogleAPI Emulator',
-            browserName: 'chrome',
-            logName: 'EmulatorSamsungGalaxyS9WQHDGoogleAPI7.1ChromeDriver',
-            platformName: 'Android',
-            platformVersion: '7.1',
-            appiumVersion: '1.18.1',
-            specs: [ mobileSpecs ],
-            build: buildName,
+            browserName: "chrome",
+            platformName: "Android",
+            "appium:deviceName": "Samsung Galaxy S9 WQHD GoogleAPI Emulator",
+            "appium:platformVersion": "7.1",
+            "appium:automationName": "UIAutomator2",
+            "wdio-ics:options": {
+                logName: "EmulatorSamsungGalaxyS9WQHDGoogleAPI7.1ChromeDriver",
+            },
+            "sauce:options": {
+                appiumVersion: "1.18.1",
+                build: buildName,
+            },
+            specs: [mobileSpecs],
         },
         {
-            deviceName: 'Google Pixel GoogleAPI Emulator',
-            browserName: 'chrome',
-            logName: 'GooglePixelGoogleAPIEmulator8.1ChromeDriver',
-            platformName: 'Android',
-            platformVersion: '8.1',
-            appiumVersion: '1.18.1',
-            specs: [ mobileSpecs ],
-            build: buildName,
+            browserName: "chrome",
+            platformName: "Android",
+            "appium:deviceName": "Google Pixel GoogleAPI Emulator",
+            "appium:platformVersion": "8.1",
+            "appium:automationName": "UIAutomator2",
+            "wdio-ics:options": {
+                logName: "GooglePixelGoogleAPIEmulator8.1ChromeDriver",
+            },
+            "sauce:options": {
+                appiumVersion: "1.18.1",
+                build: buildName,
+            },
+            specs: [mobileSpecs],
         },
-        ...['9.0', '10.0', '11.0'].map(platformVersion => ({
-            deviceName: 'Google Pixel 3 XL GoogleAPI Emulator',
-            browserName: 'chrome',
-            logName: `EmulatorGooglePixel3XLGoogleAPI${platformVersion}ChromeDriver`,
-            platformName: 'Android',
-            platformVersion: platformVersion,
-            specs: [ mobileSpecs ],
-            build: buildName,
+        ...["9.0", "10.0", "11.0"].map((platformVersion) => ({
+            browserName: "chrome",
+            platformName: "Android",
+            "appium:deviceName": "Google Pixel 3 XL GoogleAPI Emulator",
+            "appium:platformVersion": platformVersion,
+            "appium:automationName": "UIAutomator2",
+            "wdio-ics:options": {
+                logName: `EmulatorGooglePixel3XLGoogleAPI${platformVersion}ChromeDriver`,
+            },
+            "sauce:options": {
+                build: buildName,
+            },
+            specs: [mobileSpecs],
         })),
 
         /**
          * Not supporting Android Tablets
          */
-    ]
-}
+    ];
+};
--- tests/configs/wdio.saucelabs.conf.js
+++ tests/configs/wdio.saucelabs.conf.js
@@ -1,19 +1,21 @@
-const {join} = require('path');
-const SauceLabs = require('saucelabs');
-const {config} = require('./wdio.shared.conf');
-const WdioImageComparisonService = require('../../build/');
-const {sauceIosSim} = require("./sauce.ios.sims");
-const {sauceAndroidEmus} = require("./sauce.android.emus");
-const {sauceDesktopBrowsers} = require("./sauce.desktop.browsers");
+const { join } = require("path");
+const SauceLabs = require("saucelabs");
+const { config } = require("./wdio.shared.conf");
+const WdioImageComparisonService = require("../../build/");
+const sauceIosSim = require("./sauce.ios.sims");
+const sauceAndroidEmus = require("./sauce.android.emus");
+const sauceDesktopBrowsers = require("./sauce.desktop.browsers");
 
-const buildIdentifier = process.env.CI ? `${process.env.GITHUB_WORKFLOW} - ${process.env.GITHUB_JOB}` : `Local build-${new Date().getTime()}`;
+const buildIdentifier = process.env.CI
+    ? `${process.env.GITHUB_WORKFLOW} - ${process.env.GITHUB_JOB}`
+    : `Local build-${new Date().getTime()}`;
 
 // =========================
 // Sauce RDC specific config
 // =========================
 config.user = process.env.SAUCE_USERNAME_WDIO_ICS;
 config.key = process.env.SAUCE_ACCESS_KEY_WDIO_ICS;
-config.region = 'eu';
+config.region = "eu";
 
 // ============
 // Capabilities
@@ -22,40 +24,43 @@ config.capabilities = [
     /**
      * iOS
      */
-    ...sauceIosSim({
-        buildName: buildIdentifier,
-    }),
+    // ...sauceIosSim({
+    //     buildName: buildIdentifier,
+    // }),
     /**
      * Android
      */
     ...sauceAndroidEmus({
         buildName: buildIdentifier,
     }),
-    /**
-     * Desktop browser
-     */
-    ...sauceDesktopBrowsers({
-        buildName: buildIdentifier,
-    }),
+    // /**
+    //  * Desktop browser
+    //  */
+    // ...sauceDesktopBrowsers({
+    //     buildName: buildIdentifier,
+    // }),
 ];
 
 // ===================
 // Image compare setup
 // ===================
 config.services = [
-    'sauce',
-    'shared-store',
-    [WdioImageComparisonService.default, {
-        baselineFolder: join(process.cwd(), './tests/sauceLabsBaseline/'),
-        formatImageName: '{tag}-{logName}-{width}x{height}',
-        screenshotPath: join(process.cwd(), '.tmp/'),
-        savePerInstance: true,
-        autoSaveBaseline: true,
-        blockOutStatusBar: true,
-        blockOutToolBar: true,
-        n: true,
-        logLevel: 'debug',
-    }],
+    "sauce",
+    "shared-store",
+    [
+        WdioImageComparisonService.default,
+        {
+            baselineFolder: join(process.cwd(), "./tests/sauceLabsBaseline/"),
+            formatImageName: "{tag}-{logName}-{width}x{height}",
+            screenshotPath: join(process.cwd(), ".tmp/"),
+            savePerInstance: true,
+            autoSaveBaseline: true,
+            blockOutStatusBar: true,
+            blockOutToolBar: true,
+            n: true,
+            logLevel: "debug",
+        },
+    ],
 ];
 
 // If a test fails the first time and succeeds the second them, then our build would still be marked as failed.
@@ -67,30 +72,37 @@ config.services = [
 config.after = async (result, capabilities, specs) => {
     // Get the spec name path
     const specFileNamePath = specs[0];
-    const RETRIED_SPECS_KEY = 'retriedSpecs';
+    const RETRIED_SPECS_KEY = "retriedSpecs";
 
     // If the retriedSpecs array was not already created, then create it
     if (!browser.sharedStore.get(RETRIED_SPECS_KEY)) {
-        browser.sharedStore.set(RETRIED_SPECS_KEY, [])
+        browser.sharedStore.set(RETRIED_SPECS_KEY, []);
     }
 
     // The test failed and should be retried
     // Store the retry spec on the global scope
-    if ('specFileRetries' in browser.config && browser.config.specFileRetries > 0 && result === 1) {
+    if (
+        "specFileRetries" in browser.config &&
+        browser.config.specFileRetries > 0 &&
+        result === 1
+    ) {
         const retriedSpecs = browser.sharedStore.get(RETRIED_SPECS_KEY);
         retriedSpecs.push({
             sessionId: browser.sessionId,
             specFileNamePath,
-        })
-        browser.sharedStore.set(RETRIED_SPECS_KEY, retriedSpecs)
+        });
+        browser.sharedStore.set(RETRIED_SPECS_KEY, retriedSpecs);
     }
 
     // When the test succeeds
     if (result === 0) {
         // Find the test that failed before
-        const matchingSession = browser.sharedStore.get(RETRIED_SPECS_KEY).find(
-            retriedSpec => retriedSpec.specFileNamePath === specFileNamePath
-        );
+        const matchingSession = browser.sharedStore
+            .get(RETRIED_SPECS_KEY)
+            .find(
+                (retriedSpec) =>
+                    retriedSpec.specFileNamePath === specFileNamePath
+            );
         // If there is a matching session
         if (matchingSession) {
             // Then update the test in Sauce Labs with the API
@@ -100,11 +112,17 @@ config.after = async (result, capabilities, specs) => {
                 region: browser.config.region,
             });
             // We need to get the name of the job to be able to pre and post fix it
-            const jobData = await api.getJob(process.env.SAUCE_USERNAME, matchingSession.sessionId);
+            const jobData = await api.getJob(
+                process.env.SAUCE_USERNAME,
+                matchingSession.sessionId
+            );
 
             // Only update the job name and status if it hasn't been updated previously
             // The change that this will happen is very small, but this is a fail save
-            if (jobData.name && !jobData.name.includes('Succeeded after retry')) {
+            if (
+                jobData.name &&
+                !jobData.name.includes("Succeeded after retry")
+            ) {
                 // Pre and post fix the job name
                 const body = {
                     name: `❌ - ${jobData.name} - Succeeded after retry`,
