d287496bedced0e1f046185369b8f380b7e0c38c
Wim Selles <wim@wswebcreation.nl>
2022-07-30 23:48:44 +0200

    feat: add iOS 14/15 support all screens - add landscape and portrait support

--- tests/specs/mobile.spec.js
+++ tests/specs/mobile.spec.js
@@ -8,13 +8,19 @@ describe('wdio-image-comparison-service mobile', () => {
         await browser.pause(3000)
     })
 
-    // Chrome remembers the last postion when the url is loaded again, this will reset it.
+    // Chrome remembers the last position when the url is loaded again, this will reset it.
     afterEach(
         async () => await browser.executeScript('window.scrollTo(0, 0);', [])
     )
 
+    describe('compare screen', () => {
+        it('should compare successful with a mobile screenshot baseline', async () => {
+            await expect(await browser.checkScreen('screenshot')).toEqual(0)
+        })
+    })
+
     describe('compare element', () => {
-        it('should compare successful with a baseline', async () => {
+        it('should compare successful with a mobile element baseline', async () => {
             await expect(
                 await browser.checkElement(
                     await $('.uk-button:nth-child(1)'),
@@ -25,7 +31,7 @@ describe('wdio-image-comparison-service mobile', () => {
     })
 
     describe('compare full page', () => {
-        it('should compare successful with a mobile baseline', async () => {
+        it('should compare successful with a mobile full page baseline', async () => {
             await expect(
                 await browser.checkFullPageScreen('fullPage', {
                     fullPageScrollTimeout: '1500',
