16f2518f154279ba358393a9aaaf5e4906eb562e
Wim Selles <wim@wswebcreation.nl>
2022-08-01 13:48:39 +0200

    feat: add Android ChromeDriver 8-12 portrait-landscape

--- tests/configs/sauce.android.emus.js
+++ tests/configs/sauce.android.emus.js
@@ -2,90 +2,71 @@ const { join } = require('path')
 
 module.exports = function sauceAndroidEmus({ buildName }) {
     const mobileSpecs = join(process.cwd(), './tests/specs/mobile.spec.js')
+    const chromeDriverDevices = ['LANDSCAPE', 'PORTRAIT']
+        .map((orientation) =>
+            ['8.1', '9.0', '10.0', '11.0', '12.0'].map((platformVersion) => ({
+                browserName: 'chrome',
+                platformName: 'Android',
+                'appium:deviceName':
+                    // Android 8.1 needs to be on a different phone
+                    platformVersion === '8.1'
+                        ? 'Samsung Galaxy S9 WQHD GoogleAPI Emulator'
+                        : 'Google Pixel 3 XL GoogleAPI Emulator',
+                'appium:platformVersion': platformVersion,
+                'appium:automationName': 'UIAutomator2',
+                'appium:orientation': orientation,
+                'wdio-ics:options': {
+                    logName:
+                        // Android 8.1 needs to be on a different phone
+                        (platformVersion === '8.1'
+                            ? 'EmulatorSamsungGalaxyS9WQHDChromeDriver'
+                            : 'EmulatorGooglePixel3XLGoogleAPIChromeDriver') +
+                        orientation.charAt(0).toUpperCase() +
+                        orientation.slice(1).toLowerCase() +
+                        platformVersion,
+                },
+                'sauce:options': {
+                    build: buildName,
+                },
+                specs: [mobileSpecs],
+            }))
+        )
+        .flat(1)
+    console.log('devices = ', JSON.stringify(chromeDriverDevices, null, 2))
 
     return [
         /**
-         * Android with nativeWebScreenshot
+         * Android phones with nativeWebScreenshot
          */
-        {
-            'appium:deviceName': 'Google Pixel GoogleAPI Emulator',
-            browserName: 'chrome',
-            platformName: 'Android',
-            'appium:platformVersion': '8.1',
-            'appium:automationName': 'UIAutomator2',
-            'appium:nativeWebScreenshot': true,
-            'wdio-ics:options': {
-                logName: 'GooglePixelGoogleAPIEmulator8.1NativeWebScreenshot',
-            },
-            'sauce:options': {
-                appiumVersion: '1.18.1',
-                build: buildName,
-            },
-            specs: [mobileSpecs],
-        },
-        ...['9.0', '10.0', '11.0', '12.0'].map((platformVersion) => ({
-            browserName: 'chrome',
-            platformName: 'Android',
-            'appium:deviceName': 'Google Pixel 3 XL GoogleAPI Emulator',
-            'appium:platformVersion': platformVersion,
-            'appium:automationName': 'UIAutomator2',
-            'appium:nativeWebScreenshot': true,
-            'wdio-ics:options': {
-                logName: `EmulatorGooglePixel3XLGoogleAPI${platformVersion}NativeWebScreenshot`,
-            },
-            'sauce:options': {
-                build: buildName,
-            },
-            specs: [mobileSpecs],
-        })),
+        // ...['8.1', '9.0', '10.0', '11.0', '12.0'].map((platformVersion) => ({
+        //     browserName: 'chrome',
+        //     platformName: 'Android',
+        //     'appium:deviceName':
+        //         // Android 8.1 needs to be aon a different phone
+        //         platformVersion === '8.1'
+        //             ? 'Samsung Galaxy S9 WQHD GoogleAPI Emulator'
+        //             : 'Google Pixel 3 XL GoogleAPI Emulator',
+        //     'appium:platformVersion': platformVersion,
+        //     'appium:automationName': 'UIAutomator2',
+        //     'appium:orientation': 'PORTRAIT',
+        //     'appium:nativeWebScreenshot': true,
+        //     'wdio-ics:options': {
+        //         logName:
+        //             // Android 8.1 needs to be aon a different phone
+        //             platformVersion === '8.1'
+        //                 ? `EmulatorSamsungGalaxyS9WQHD${platformVersion}NativeWebScreenshot`
+        //                 : `EmulatorGooglePixel3XLGoogleAPI${platformVersion}NativeWebScreenshot`,
+        //     },
+        //     'sauce:options': {
+        //         build: buildName,
+        //     },
+        //     specs: [mobileSpecs],
+        // })),
 
         /**
-         * Android with chrome driver screenshots
+         * Android phones with ChromeDriver screenshots
          */
-        {
-            browserName: 'chrome',
-            platformName: 'Android',
-            'appium:deviceName': 'Samsung Galaxy S9 WQHD GoogleAPI Emulator',
-            'appium:platformVersion': '7.1',
-            'appium:automationName': 'UIAutomator2',
-            'wdio-ics:options': {
-                logName: 'EmulatorSamsungGalaxyS9WQHDGoogleAPI7.1ChromeDriver',
-            },
-            'sauce:options': {
-                appiumVersion: '1.18.1',
-                build: buildName,
-            },
-            specs: [mobileSpecs],
-        },
-        {
-            browserName: 'chrome',
-            platformName: 'Android',
-            'appium:deviceName': 'Google Pixel GoogleAPI Emulator',
-            'appium:platformVersion': '8.1',
-            'appium:automationName': 'UIAutomator2',
-            'wdio-ics:options': {
-                logName: 'GooglePixelGoogleAPIEmulator8.1ChromeDriver',
-            },
-            'sauce:options': {
-                appiumVersion: '1.18.1',
-                build: buildName,
-            },
-            specs: [mobileSpecs],
-        },
-        ...['9.0', '10.0', '11.0', '12.0'].map((platformVersion) => ({
-            browserName: 'chrome',
-            platformName: 'Android',
-            'appium:deviceName': 'Google Pixel 3 XL GoogleAPI Emulator',
-            'appium:platformVersion': platformVersion,
-            'appium:automationName': 'UIAutomator2',
-            'wdio-ics:options': {
-                logName: `EmulatorGooglePixel3XLGoogleAPI${platformVersion}ChromeDriver`,
-            },
-            'sauce:options': {
-                build: buildName,
-            },
-            specs: [mobileSpecs],
-        })),
+        ...chromeDriverDevices,
 
         /**
          * Not supporting Android Tablets
