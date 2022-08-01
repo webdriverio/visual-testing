740af2c7233fe29382565d7884cc3a6ff358aa10
Wim Selles <wim@wswebcreation.nl>
2022-08-01 15:34:56 +0200

    feat: add proper chromedriver landscape support

--- tests/configs/sauce.android.emus.js
+++ tests/configs/sauce.android.emus.js
@@ -4,35 +4,23 @@ module.exports = function sauceAndroidEmus({ buildName }) {
     const mobileSpecs = join(process.cwd(), './tests/specs/mobile.spec.js')
     const chromeDriverDevices = ['LANDSCAPE', 'PORTRAIT']
         .map((orientation) =>
-            ['8.1', '9.0', '10.0', '11.0', '12.0'].map((platformVersion) => ({
-                browserName: 'chrome',
-                platformName: 'Android',
-                'appium:deviceName':
-                    // Android 8.1 needs to be on a different phone
-                    platformVersion === '8.1'
-                        ? 'Samsung Galaxy S9 WQHD GoogleAPI Emulator'
-                        : 'Google Pixel 3 XL GoogleAPI Emulator',
-                'appium:platformVersion': platformVersion,
-                'appium:automationName': 'UIAutomator2',
-                'appium:orientation': orientation,
-                'wdio-ics:options': {
-                    logName:
-                        // Android 8.1 needs to be on a different phone
-                        (platformVersion === '8.1'
-                            ? 'EmulatorSamsungGalaxyS9WQHDChromeDriver'
-                            : 'EmulatorGooglePixel3XLGoogleAPIChromeDriver') +
-                        orientation.charAt(0).toUpperCase() +
-                        orientation.slice(1).toLowerCase() +
-                        platformVersion,
-                },
-                'sauce:options': {
-                    build: buildName,
-                },
-                specs: [mobileSpecs],
-            }))
+            ['8.1', '9.0', '10.0', '11.0', '12.0'].map((platformVersion) =>
+                createCaps({
+                    deviceName:
+                        platformVersion === '8.1'
+                            ? 'Samsung Galaxy S9 WQHD GoogleAPI Emulator'
+                            : 'Google Pixel 3 XL GoogleAPI Emulator',
+                    platformVersion: platformVersion,
+                    orientation: orientation,
+                    mobileSpecs,
+                    sauceOptions: {
+                        build: buildName,
+                        deviceOrientation: orientation,
+                    },
+                })
+            )
         )
         .flat(1)
-    console.log('devices = ', JSON.stringify(chromeDriverDevices, null, 2))
 
     return [
         /**
@@ -72,4 +60,36 @@ module.exports = function sauceAndroidEmus({ buildName }) {
          * Not supporting Android Tablets
          */
     ]
+}
+
+function createCaps({
+    deviceName,
+    mobileSpecs,
+    nativeWebScreenshot = false,
+    orientation,
+    platformVersion,
+    sauceOptions,
+}) {
+    const driverScreenshotType = nativeWebScreenshot
+        ? 'NativeWebScreenshot'
+        : 'ChromeDriver'
+    return {
+        browserName: 'chrome',
+        platformName: 'Android',
+        'appium:deviceName': deviceName,
+        'appium:platformVersion': platformVersion,
+        'appium:orientation': orientation,
+        ...(nativeWebScreenshot ? { 'appium:nativeWebScreenshot': true } : {}),
+        'appium:automationName': 'UIAutomator2',
+        'wdio-ics:options': {
+            logName: `Emulator${deviceName.replace(
+                /(\s+|\(+|\)+|Emulator)/g,
+                ''
+            )}${orientation.charAt(0).toUpperCase()}${orientation
+                .slice(1)
+                .toLowerCase()}${driverScreenshotType}${platformVersion}`,
+        },
+        'sauce:options': sauceOptions,
+        specs: [mobileSpecs],
+    }
 }
