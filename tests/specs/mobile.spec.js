2923e7db51b99d230ae54a638dcb310f6229e3df
Wim Selles <wim@wswebcreation.nl>
2022-10-09 09:32:37 +0200

    feat: add ios-bezel images and new spec file

--- tests/specs/mobile.spec.js
+++ tests/specs/mobile.spec.js
@@ -4,6 +4,9 @@
 describe('wdio-image-comparison-service mobile', () => {
     const deviceName = driver.capabilities.deviceName
     const orientation = driver.capabilities.orientation
+    // Get the commands that need to be executed
+    // 0 means all, otherwise it will only execute the commands that are specified
+    const wdioIcsCommands = driver.capabilities['wdio-ics:options'].commands
 
     beforeEach(async () => {
         await browser.url('')
@@ -16,24 +19,39 @@ describe('wdio-image-comparison-service mobile', () => {
         async () => await browser.executeScript('window.scrollTo(0, 0);', [])
     )
 
-    it(`should compare a screen successful for '${deviceName}' in ${orientation}-mode`, async () => {
-        await expect(await browser.checkScreen('screenshot')).toEqual(0)
-    })
+    if (
+        wdioIcsCommands.length === 0 ||
+        wdioIcsCommands.includes('checkScreen')
+    ) {
+        it(`should compare a screen successful for '${deviceName}' in ${orientation}-mode`, async () => {
+            await expect(await browser.checkScreen('screenshot')).toEqual(0)
+        })
+    }
 
-    it(`should compare an element successful for '${deviceName}' in ${orientation}-mode`, async () => {
-        await expect(
-            await browser.checkElement(
-                await $('.uk-button:nth-child(1)'),
-                'firstButtonElement'
-            )
-        ).toEqual(0)
-    })
+    if (
+        wdioIcsCommands.length === 0 ||
+        wdioIcsCommands.includes('checkElement')
+    ) {
+        it(`should compare an element successful for '${deviceName}' in ${orientation}-mode`, async () => {
+            await expect(
+                await browser.checkElement(
+                    await $('.uk-button:nth-child(1)'),
+                    'firstButtonElement'
+                )
+            ).toEqual(0)
+        })
+    }
 
-    it(`should compare a full page screenshot successful for '${deviceName}' in ${orientation}-mode`, async () => {
-        await expect(
-            await browser.checkFullPageScreen('fullPage', {
-                fullPageScrollTimeout: '1500',
-            })
-        ).toEqual(0)
-    })
+    if (
+        wdioIcsCommands.length === 0 ||
+        wdioIcsCommands.includes('checkFullPageScreen')
+    ) {
+        it(`should compare a full page screenshot successful for '${deviceName}' in ${orientation}-mode`, async () => {
+            await expect(
+                await browser.checkFullPageScreen('fullPage', {
+                    fullPageScrollTimeout: '1500',
+                })
+            ).toEqual(0)
+        })
+    }
 })
