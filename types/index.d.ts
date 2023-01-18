167df37aeaed3eec558723b81da4a0187798500f
Philippe Sabourin <philippe.sabourin@sas.com>
2023-01-18 11:25:27 -0500

    Fixing the browser type with the generic instead so it can work in all cases

--- types/index.d.ts
+++ types/index.d.ts
@@ -61,7 +61,7 @@ type Result = MultiResult | (ImageCompareResult | number)
 
 declare global {
     namespace WebdriverIO {
-        interface Browser {
+        interface Browser<Mode> {
             /**
              * Saves an image of an element
              */
