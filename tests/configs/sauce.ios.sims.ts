08016427fbda412624d0f14c8bfda49409a437e6
Wim Selles <wim@wswebcreation.nl>
2023-10-29 09:13:41 +0100

    breaking: dropped support for iOS 13

--- tests/configs/sauce.ios.sims.ts
+++ tests/configs/sauce.ios.sims.ts
@@ -8,32 +8,6 @@ export function sauceIosSim({ buildName }: { buildName: string }) {
     const mobileSpecs = join(process.cwd(), './tests/specs/mobile.spec.ts')
     // For all screen sizes see
     // https://github.com/wswebcreation/webdriver-image-comparison/blob/main/lib/helpers/constants.ts
-    const iOS13Devices = [
-        // 667
-        'iPhone 8 Simulator',
-        // 736
-        'iPhone 8 Plus Simulator',
-        // 812
-        'iPhone XS Simulator',
-        // 844 => not available for iOS 13
-        // 896
-        'iPhone 11 Simulator',
-        // 926 => not available for iOS 13
-        // 1024
-        'iPad Air 2 Simulator',
-        // 1080
-        'iPad (5th generation) Simulator',
-        // 1112
-        'iPad Pro (10.5 inch) Simulator',
-        // 1133 => not available for iOS 13
-        // 1180 => not available for iOS 13
-        // 1194
-        'iPad Pro (11 inch) (2nd generation) Simulator',
-        // 1366
-        'iPad Pro (12.9 inch) Simulator',
-        // 1366
-        'iPad Pro (12.9 inch) (4th generation) Simulator',
-    ]
     const iOS14Devices = [
         // 667
         'iPhone 8 Simulator',
@@ -158,23 +132,6 @@ export function sauceIosSim({ buildName }: { buildName: string }) {
     ]
 
     return [
-        // For some reason with iOS 13 the Landscape screenshot is not correct
-        ...(['LANDSCAPE', 'PORTRAIT'] as DeviceOrientation[])
-            .map((orientation) =>
-                iOS13Devices.map((device) =>
-                    createCaps({
-                        deviceName: device,
-                        platformVersion: '13.4',
-                        orientation: orientation,
-                        mobileSpecs,
-                        sauceOptions: {
-                            build: buildName,
-                            deviceOrientation: orientation,
-                        },
-                    })
-                )
-            )
-            .flat(1),
         ...(['LANDSCAPE', 'PORTRAIT'] as DeviceOrientation[])
             .map((orientation) =>
                 iOS14Devices.map((device) =>
@@ -196,7 +153,7 @@ export function sauceIosSim({ buildName }: { buildName: string }) {
                 iOS15Devices.map((device) =>
                     createCaps({
                         deviceName: device,
-                        platformVersion: '15.4',
+                        platformVersion: '15.5',
                         orientation: orientation,
                         mobileSpecs,
                         sauceOptions: {
@@ -212,12 +169,11 @@ export function sauceIosSim({ buildName }: { buildName: string }) {
                 iOS16Devices.map((device) =>
                     createCaps({
                         deviceName: device,
-                        platformVersion: '16',
+                        platformVersion: '16.2',
                         orientation: orientation,
                         mobileSpecs,
                         sauceOptions: {
                             build: buildName,
-                            appiumVersion: '2.0.0-beta44',
                             deviceOrientation: orientation,
                         },
                         wdioIcsCommands: [
@@ -233,12 +189,11 @@ export function sauceIosSim({ buildName }: { buildName: string }) {
                 iOS16BezelDevices.map((device) =>
                     createCaps({
                         deviceName: device,
-                        platformVersion: '16',
+                        platformVersion: '16.2',
                         orientation: orientation,
                         mobileSpecs,
                         sauceOptions: {
                             build: buildName,
-                            appiumVersion: '2.0.0-beta44',
                             deviceOrientation: orientation,
                         },
                         wdioIcsCommands: ['checkScreen'],
@@ -291,7 +246,10 @@ function createCaps({
             ),
             commands: wdioIcsCommands,
         },
-        'sauce:options': sauceOptions,
+        'sauce:options': {
+            ...sauceOptions,
+            appiumVersion: '2.0.0',
+        },
         specs: [mobileSpecs],
     }
 }
