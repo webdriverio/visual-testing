d9cc5061cd4c8bc755b40a9cb6e156fc711f5385
wswebcreation <wim@wswebcreation.nl>
2024-02-19 09:54:39 +0100

    test: add mobile images
    - add android
    - add iOS
    - reduce screenshots for iOS

--- tests/specs/mobile.web.spec.ts
+++ tests/specs/mobile.web.spec.ts
@@ -32,17 +32,11 @@ describe('@wdio/visual-service mobile web', () => {
             // This is normally a bad practice, but a mobile screenshot is normally around 1M pixels
             // We're accepting 0.05%, which is 500 pixels, to be a max difference
             const result = await browser.checkScreen('screenshot') as number
+            if (result > 0 && result < 0.05) {
+                console.log(`\n\n\n'Screenshot for ${deviceName}' in ${orientation}-mode has a difference of ${result}%\n\n\n`)
+            }
             await expect(result < 0.05 ? 0 : result).toEqual(0)
         })
-
-        if (orientation === 'portrait') {
-            it(`should compare a screen layout successful for '${deviceName}' in ${orientation}-mode`, async () => {
-            // This is normally a bad practice, but a mobile screenshot is normally around 1M pixels
-            // We're accepting 0.05%, which is 500 pixels, to be a max difference
-                const result = await browser.checkScreen('layoutScreenshot', { enableLayoutTesting: true }) as number
-                await expect(result < 0.05 ? 0 : result).toEqual(0)
-            })
-        }
     }
 
     if (
@@ -60,20 +54,6 @@ describe('@wdio/visual-service mobile web', () => {
                 )
             ).toEqual(0)
         })
-
-        if (orientation === 'portrait') {
-            it(`should compare an element layout successful for '${deviceName}' in ${orientation}-mode`, async () => {
-                await expect(
-                    await browser.checkElement(
-                        await $('nav.navbar'),
-                        'wdioLayoutNavBar',
-                        {
-                            enableLayoutTesting: true
-                        }
-                    )
-                ).toEqual(0)
-            })
-        }
     }
 
     if (
@@ -89,22 +69,10 @@ describe('@wdio/visual-service mobile web', () => {
                     await $('nav.navbar'),
                 ],
             }) as number
+            if (result > 0 && result < 0.05) {
+                console.log(`\n\n\nFull page layout screenshot for '${deviceName}' in ${orientation}-mode has a difference of ${result}%\n\n\n`)
+            }
             await expect(result < 0.05 ? 0 : result).toEqual(0)
         })
-
-        if (orientation === 'portrait') {
-            it(`should compare a full page layout screenshot successful for '${deviceName}' in ${orientation}-mode`, async () => {
-            // This is normally a bad practice, but a mobile full page screenshot is normally around 4M pixels
-            // We're accepting 0.05%, which is 2000 pixels, to be a max difference
-                const result = await browser.checkFullPageScreen('fullPageLayout', {
-                    fullPageScrollTimeout: 1500,
-                    hideAfterFirstScroll: [
-                        await $('nav.navbar'),
-                    ],
-                    enableLayoutTesting: true
-                }) as number
-                await expect(result < 0.05 ? 0 : result).toEqual(0)
-            })
-        }
     }
 })
