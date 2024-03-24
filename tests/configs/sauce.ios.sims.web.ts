259e00c1865760787683f6de0e9f9dcb2eb41b21
wswebcreation <wim@wswebcreation.nl>
2024-03-24 15:15:39 +0100

    test: reduce iOS versions

--- tests/configs/sauce.ios.sims.web.ts
+++ tests/configs/sauce.ios.sims.web.ts
@@ -8,53 +8,55 @@ export function sauceIosSimWeb({ buildName }: { buildName: string }) {
     const mobileSpecs = join(process.cwd(), './tests/specs/mobile.web.spec.ts')
     // For all screen sizes see
     // https://github.com/wswebcreation/webdriver-image-comparison/blob/main/lib/helpers/constants.ts
+    // The strategy for selecting which devices to use it to:
+    // - Support the latest 3 major iOS versions
+    // - For the latest iOS version check all screen sizes from the constants file for the latest OS in landscape and portrait
+    // - only check the released devices for latest-1 and latest -2 for a phone and a tablet in portrait due to risk on landscape
+    // - add missing smaller screen sizes for the latest-1 and latest-2
     const iOS15Devices = [
-        // 667
-        'iPhone 8 Simulator',
         // 736
         'iPhone 8 Plus Simulator',
         // 812
-        'iPhone XS Simulator',
+        'iPhone 13 mini Simulator',
         // 844
-        'iPhone 12 Simulator',
-        // 896
-        'iPhone 11 Simulator',
+        'iPhone 13 Pro Simulator', // Same as the iPhone 13
         // 926
-        'iPhone 12 Pro Max Simulator',
-        // 1024
-        'iPad Air 2 Simulator',
+        'iPhone 13 Pro Max Simulator',
         // 1080
-        'iPad (5th generation) Simulator',
-        // 1112
-        'iPad Pro (10.5 inch) Simulator',
-        // 1133
+        'iPad (9th generation) Simulator',
+        // 1024
         'iPad mini (6th generation) Simulator',
-        // 1180
-        'iPad Air (4th generation) Simulator',
-        // 1194
-        'iPad Pro (11 inch) (3rd generation) Simulator',
-        // 1366
-        'iPad Pro (12.9 inch) (1st generation) Simulator',
-        // 1366
-        'iPad Pro (12.9 inch) (5th generation) Simulator',
     ]
     const iOS16Devices = [
+        // 844
+        'iPhone 14 Simulator',
+        // 852
+        'iPhone 14 Pro Simulator',
+        // 926
+        'iPhone 14 Plus Simulator',
+        // 932
+        'iPhone 14 Pro Max Simulator',
+        // No new iPads were released with iOS 16
+    ]
+    const iOS17Devices = [
         // 667
-        'iPhone 8 Simulator',
-        // 736
-        'iPhone 8 Plus Simulator',
+        'iPhone SE (3rd generation) Simulator',
         // 812
         'iPhone XS Simulator',
         // 844
-        'iPhone 12 Simulator',
+        'iPhone 14 Simulator',
+        // 852
+        'iPhone 15 Simulator', // or iPhone 15 Pro
         // 896
         'iPhone 11 Simulator',
         // 926
         'iPhone 12 Pro Max Simulator',
+        // 932
+        'iPhone 15 Pro Max Simulator', // or iPhone 15 Plus
         // 1024
         'iPad mini (5th generation) Simulator',
         // 1080
-        'iPad (5th generation) Simulator',
+        'iPad (10th generation) Simulator',
         // 1112
         'iPad Pro (10.5 inch) Simulator',
         // 1133
@@ -63,14 +65,12 @@ export function sauceIosSimWeb({ buildName }: { buildName: string }) {
         'iPad Air (4th generation) Simulator',
         // 1194
         'iPad Pro (11 inch) (3rd generation) Simulator',
-        // 1366
-        'iPad Pro (12.9 inch) (1st generation) Simulator',
         // 1366
         'iPad Pro (12.9 inch) (5th generation) Simulator',
     ]
 
     return [
-        ...(['LANDSCAPE', 'PORTRAIT'] as DeviceOrientation[])
+        ...(['PORTRAIT'] as DeviceOrientation[])
             .map((orientation) =>
                 iOS15Devices.map((device) =>
                     createCaps({
@@ -79,6 +79,29 @@ export function sauceIosSimWeb({ buildName }: { buildName: string }) {
                         orientation: orientation,
                         mobileSpecs,
                         sauceOptions: {
+                            appiumVersion: '2.0.0',
+                            build: buildName,
+                            deviceOrientation: orientation,
+                        },
+                        wdioIcsCommands: [
+                            'checkScreen',
+                            // 'checkElement', // Disabled because it's flaky on Sauce Labs
+                            'checkFullPageScreen',
+                        ],
+                    })
+                )
+            )
+            .flat(1),
+        ...(['PORTRAIT'] as DeviceOrientation[])
+            .map((orientation) =>
+                iOS16Devices.map((device) =>
+                    createCaps({
+                        deviceName: device,
+                        platformVersion: '16.2',
+                        orientation: orientation,
+                        mobileSpecs,
+                        sauceOptions: {
+                            appiumVersion: '2.0.0',
                             build: buildName,
                             deviceOrientation: orientation,
                         },
@@ -93,13 +116,14 @@ export function sauceIosSimWeb({ buildName }: { buildName: string }) {
             .flat(1),
         ...(['LANDSCAPE', 'PORTRAIT'] as DeviceOrientation[])
             .map((orientation) =>
-                iOS16Devices.map((device) =>
+                iOS17Devices.map((device) =>
                     createCaps({
                         deviceName: device,
-                        platformVersion: '16.2',
+                        platformVersion: '17.0',
                         orientation: orientation,
                         mobileSpecs,
                         sauceOptions: {
+                            appiumVersion: '2.1.3',
                             build: buildName,
                             deviceOrientation: orientation,
                         },
@@ -127,6 +151,7 @@ function createCaps({
     sauceOptions,
 }:{
     wdioIcsCommands?: string[],
+    appiumVersion?: string,
     deviceName: string,
     mobileSpecs: string,
     orientation: DeviceOrientation,
@@ -159,7 +184,6 @@ function createCaps({
         },
         'sauce:options': {
             ...sauceOptions,
-            appiumVersion: '2.0.0',
         },
         specs: [mobileSpecs],
     }
