aeeb7cac52e50085d69c11168a3a4df1ea3e30f0
wswebcreation <wim@wswebcreation.nl>
2024-04-14 10:11:21 +0200

    tesT: update e2e test

--- tests/specs/desktop.ocr.spec.ts
+++ tests/specs/desktop.ocr.spec.ts
@@ -1,30 +1,15 @@
 import { browser } from '@wdio/globals'
 
 describe('@wdio/visual-service:ocr desktop', () => {
-    // @TODO
-    // @ts-ignore
-    const browserName = `${browser.capabilities.browserName}-${browser.capabilities.browserVersion}`
-
     beforeEach(async () => {
         await browser.url('https://webdriver.io/')
         await $('.hero__title').waitForDisplayed()
-        // There is an issue with Safari 16 on Sauce Labs, it doesn't always load the page correctly
-        // this suite retry should result in a successful test run, otherwise the specFileRetries will retry the whole file
-        // in a new session
-        if (browserName === 'safari-16') {
-            // This is like firing with a bazooka, but it's the only way to make it work
-            // @ts-ignore
-            this.retries(5)
-            // For some reason the safari 16 browser on Sauce Labs doesn't load the page correctly for the first try
-            await browser.url('https://webdriver.io/')
-            await $('.hero__title').waitForDisplayed()
-        }
     })
 
     // Chrome remembers the last position when the url is loaded again, this will reset it.
     afterEach(async () => await browser.execute('window.scrollTo(0, 0);', []))
 
-    it(`should get text of an image based on OCR '${browserName}'`, async function() {
+    it('should get text of an image based on OCR', async function() {
         const ocrText = await driver.ocrGetText({
             element: $('.hero__subtitle'),
         })
@@ -32,7 +17,7 @@ describe('@wdio/visual-service:ocr desktop', () => {
         expect(ocrText).toMatchSnapshot()
     })
 
-    it(`should get the position of a text on the screen based on OCR '${browserName}'`, async function() {
+    it('should get the position of a text on the screen based on OCR', async function() {
         const elementPosition = await driver.ocrGetElementPositionByText({
             element: $('.DocSearch'),
             text: 'Search',
@@ -41,7 +26,7 @@ describe('@wdio/visual-service:ocr desktop', () => {
         expect(elementPosition).toMatchSnapshot()
     })
 
-    it(`should click on an element based on text on the screen based on OCR '${browserName}'`, async function() {
+    it('should click on an element based on text on the screen based on OCR', async function() {
         await driver.ocrClickOnText({
             element: $('.DocSearch'),
             text: 'Search',
@@ -55,7 +40,7 @@ describe('@wdio/visual-service:ocr desktop', () => {
         expect(ocrText).toContain('docs')
     })
 
-    it(`should set a value on an element based on text on the screen based on OCR '${browserName}'`, async function() {
+    it('should set a value on an element based on text on the screen based on OCR', async function() {
         await driver.ocrClickOnText({
             element: $('.DocSearch'),
             text: 'Search',
@@ -70,11 +55,10 @@ describe('@wdio/visual-service:ocr desktop', () => {
         await driver.pause(1000)
 
         const ocrText = await driver.ocrGetText({ element: $('.DocSearch-Form') })
-        console.log(ocrText)
         expect(ocrText).toContain('specfileretries')
     })
 
-    it(`should wait on text on the screen based on OCR '${browserName}'`, async function() {
+    it('should wait on text on the screen based on OCR', async function() {
         await driver.ocrClickOnText({
             element: $('.DocSearch'),
             text: 'Search',
