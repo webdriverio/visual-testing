08a9112c22815ff5c6620c7f301c6b7132b3d64b
wswebcreation <wim@wswebcreation.nl>
2024-11-07 14:22:52 +0100

    fix: fix using relative paths

--- packages/visual-reporter/scripts/cli.ts
+++ packages/visual-reporter/scripts/cli.ts
@@ -14,8 +14,8 @@ import { copyDirectory } from './utils/fileHandling.js'
 async function main() {
     //
     // Set some initial variables
-    let filePath = getArgValue('--jsonOutput')
-    let reportPath = getArgValue('--reportFolder')
+    const filePath = getArgValue('--jsonOutput') ? resolve(process.cwd(), getArgValue('--jsonOutput')) : undefined
+    const reportPath = getArgValue('--reportFolder') ? resolve(process.cwd(), getArgValue('--reportFolder')) : undefined
     const logLevel = getArgValue('--logLevel')
     const __filename = fileURLToPath(import.meta.url)
     const __dirname = dirname(__filename)
