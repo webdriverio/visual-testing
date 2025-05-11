60e5f4a398323c99ba53a0885de4661a813ec015
wswebcreation <wim@wswebcreation.nl>
2025-05-11 19:46:08 +0200

    chore: add proper js doc for methods

--- packages/webdriver-image-comparison/src/commands/check.interfaces.ts
+++ packages/webdriver-image-comparison/src/commands/check.interfaces.ts
@@ -7,33 +7,73 @@ import type { CheckScreenOptions } from './screen.interfaces.js'
 import type { CheckTabbableOptions } from './tabbable.interfaces.js'
 
 export interface CheckMethodOptions {
-    // Block out array with x, y, width and height values
+    /**
+     * Block out array with x, y, width and height values
+     */
     blockOut?: RectanglesOutput[];
-    // Block out the side bar on iOS iPads in landscape mode
+    /**
+     * Block out the side bar on iOS iPads in landscape mode
+     * @default false
+     */
     blockOutSideBar?: boolean;
-    // Block out the status bar yes or no
+    /**
+     * Block out the status bar yes or no
+     * @default false
+     */
     blockOutStatusBar?: boolean;
-    // Block out the tool bar yes or no
+    /**
+     * Block out the tool bar yes or no
+     * @default false
+     */
     blockOutToolBar?: boolean;
-    // Ignore elements and or areas
+    /**
+     * Ignore elements and or areas
+     */
     ignore?: (RectanglesOutput | WebdriverIO.Element | ChainablePromiseElement)[];
-    // Compare images and discard alpha
+    /**
+     * Compare images and discard alpha
+     * @default false
+     */
     ignoreAlpha?: boolean;
-    // Compare images an discard anti aliasing
+    /**
+     * Compare images an discard anti aliasing
+     * @default false
+     */
     ignoreAntialiasing?: boolean;
-    // Even though the images are in color, the comparison wil compare 2 black/white images
+    /**
+     * Even though the images are in color, the comparison wil compare 2 black/white images
+     * @default false
+     */
     ignoreColors?: boolean;
-    // Compare images and compare with red = 16, green = 16, blue = 16, alpha = 16, minBrightness=16, maxBrightness=240
+    /**
+     * Compare images and compare with red = 16, green = 16, blue = 16, alpha = 16, minBrightness=16, maxBrightness=240
+     * @default false
+     */
     ignoreLess?: boolean;
-    // Compare images and compare with red = 0, green = 0, blue = 0, alpha = 0, minBrightness=0, maxBrightness=255
+    /**
+     * Compare images and compare with red = 0, green = 0, blue = 0, alpha = 0, minBrightness=0, maxBrightness=255
+     * @default false
+     */
     ignoreNothing?: boolean;
-    // Default false. If true, return percentage will be like 0.12345678, default is 0.12
+    /**
+     * Default false. If true, return percentage will be like 0.12345678, default is 0.12
+     * @default false
+     */
     rawMisMatchPercentage?: boolean;
-    // Return all the compare data object
+    /**
+     * Return all the compare data object
+     * @default false
+     */
     returnAllCompareData?: boolean;
-    // Allowable value of misMatchPercentage that prevents saving image with
+    /**
+     * Allowable value of misMatchPercentage that prevents saving image with differences
+     * @default 0
+     */
     saveAboveTolerance?: number;
-    //Scale images to same size before comparison
+    /**
+     * Scale images to same size before comparison
+     * @default false
+     */
     scaleImagesToSameSize?: boolean;
 }
 
