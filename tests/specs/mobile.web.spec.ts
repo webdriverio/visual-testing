bb80bdd419db16fbd3736c31a760667d4373f9f8
wswebcreation <wim@wswebcreation.nl>
2026-03-02 22:03:57 +0100

    chore: update tests

--- tests/specs/mobile.web.spec.ts
+++ tests/specs/mobile.web.spec.ts
@@ -176,6 +176,8 @@ describe('@wdio/visual-service mobile web', () => {
                     ignore: [
                         await $$('.feature_G9wp h3'),
                     ],
+                    // We need to add some padding to the ignore regions
+                    // to make sure that we cover the element that is being ignored.
                     ignoreRegionPadding: 5,
                     fullPageScrollTimeout: 1500,
                     hideAfterFirstScroll: [
